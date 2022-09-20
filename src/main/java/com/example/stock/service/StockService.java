1de88257f443be50de576c9fa21e2b2848ecf7d6
이승민 <sml7179@naver.com>
2022-09-20 23:43:21 +0900

    named Lock

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,8 +3,9 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 @Service
 public class StockService {
@@ -14,7 +15,7 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    //@Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
         Stock stock = stockRepository.findById(id).orElseThrow();
 
