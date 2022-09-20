a0aaced3e3d6fe4016ae5f2af8a6020f63c78e15
이승민 <sml7179@naver.com>
2022-09-20 23:07:16 +0900

    test: 100개를 한번에 재고 감소 해보기

--- src/test/java/com/example/stock/service/StockServiceTest.java
+++ src/test/java/com/example/stock/service/StockServiceTest.java
@@ -9,6 +9,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,5 +44,29 @@ class StockServiceTest {
         Stock stock = stockRepository.findById(1L).orElseThrow();
 
         assertThat(stock.getQuantity()).isEqualTo(99L);
+    }
+
+    @Test
+    public void 동시에_100명이_주문() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount); //카운트해주는 클래스
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        // 100 - (100 * 1) = 0
+        assertThat(stock.getQuantity()).isEqualTo(0L);
     }
 }
